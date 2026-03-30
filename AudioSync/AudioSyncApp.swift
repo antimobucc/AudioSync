2e21db124e450034a088d34728f2f8eadb96aba7
antimobucciero <antimobucciero23@gmail.com>
2026-03-30 10:42:29 +0200

    Initial Commit

---
+++ AudioSync/AudioSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioSyncApp.swift
+//  AudioSync
+//
+//  Created by Antimo Bucciero on 30/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct AudioSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
